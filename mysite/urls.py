d4877996815d68ebb983fa3120fcba37680a2b4f
Sergey_Osipov <sibkron@mail.ru>
2022-07-04 21:49:44 +0700

    django project: lesson 6

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webexample/', include('webexample.urls')),
     path('', include('mainApp.urls')),
 ]
--- webexample/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebexampleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webexample'
