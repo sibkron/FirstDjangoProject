4c6eaa543329640c582d0f3aefe9f224cde25f77
Sergey_Osipov <sibkron@mail.ru>
2022-07-05 12:46:08 +0700

    django's learning: lesson 7

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainApp/homePage.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['some', 'hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'football'
+        }
+    }
+
+    return render(request, 'mainApp/homePage.html', data)
+
+
+def about(request):
+    return render(request, 'mainApp/about.html')
