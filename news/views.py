a065bfbcdf0f73c7a091582795d800d93371ddd7
Sergey_Osipov <sibkron@mail.ru>
2022-07-07 17:11:37 +0700

    django lesson 9

--- news/views.py
+++ news/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 
 def news_home(request):
     news = Articles.objects.order_by('date')
     return render(request, "news/news_home.html", {'news': news})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form
+    }
+
+    return render(request, "news/create.html", data)
